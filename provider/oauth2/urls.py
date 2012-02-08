66dc8f5a87be6077154ecb1eae46321c0575f4ec
Alen Mujezinovic <flashingpumpkin@gmail.com>
2012-02-08 18:00:32 +0000

    Added csrf exemption for the access token endpoint

--- provider/oauth2/urls.py
+++ provider/oauth2/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from provider.oauth2.views import Authorize, Redirect, Capture, AccessTokenView
 
 
@@ -7,5 +8,5 @@ urlpatterns = patterns('',
     url('^authorize/$', login_required(Capture.as_view()), name='authorize'),
     url('^authorize/2/$', login_required(Authorize.as_view()), name='authorize-2'),
     url('^redirect/$', login_required(Redirect.as_view()), name='redirect'),
-    url('^access_token/$', AccessTokenView.as_view(), name='access_token'),
+    url('^access_token/$', csrf_exempt(AccessTokenView.as_view()), name='access_token'),
 )
