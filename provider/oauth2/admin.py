6b0faa4a7a81fb91b846e5c2be7f608b70c1af79
Alen Mujezinovic <flashingpumpkin@gmail.com>
2012-02-28 12:25:50 +0000

    Admin updates, version bump

--- provider/oauth2/admin.py
+++ provider/oauth2/admin.py
@@ -4,12 +4,15 @@ from provider.oauth2.models import AccessToken, Grant, Client, \
     
 class AccessTokenAdmin(admin.ModelAdmin):
     list_display = ('user', 'client', 'token', 'expires', 'scope',)
+    raw_id_fields = ('user',)
 
 class GrantAdmin(admin.ModelAdmin):
     list_display = ('user', 'client', 'code', 'expires',)
+    raw_id_fields = ('user',)
     
 class ClientAdmin(admin.ModelAdmin):    
-    list_display = ('user', 'url', 'redirect_uri', 'client_id', 'client_type')
+    list_display = ('url', 'user', 'redirect_uri', 'client_id', 'client_type')
+    raw_id_fields = ('user',)
 
 admin.site.register(AccessToken, AccessTokenAdmin)
 admin.site.register(Grant, GrantAdmin)
