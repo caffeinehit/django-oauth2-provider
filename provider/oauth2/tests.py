3624291d85438738f924a4c7c9f3ae20f8008401
Pierre Dulac <dulacpier@gmail.com>
2013-03-12 15:52:40 +0100

    Add some tests

--- provider/oauth2/tests.py
+++ provider/oauth2/tests.py
@@ -1,5 +1,6 @@
 import json
 import urlparse
+import datetime
 from django.http import QueryDict
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -196,6 +197,15 @@ class AuthorizationTest(BaseOAuth2TestCase):
 
 class AccessTokenTest(BaseOAuth2TestCase):
     fixtures = ['test_oauth2.json']
+
+    def test_access_token_get_expire_delta_value(self):
+        user = self.get_user()
+        client = self.get_client()
+        token = AccessToken.objects.create(user=user, client=client)
+        now = datetime.datetime.now()
+        default_expiration_timedelta = constants.EXPIRE_DELTA
+        current_expiration_timedelta = datetime.timedelta(seconds=token.get_expire_delta(reference=now))
+        self.assertTrue(abs(current_expiration_timedelta - default_expiration_timedelta) <= datetime.timedelta(seconds=1))
 
     def test_fetching_access_token_with_invalid_client(self):
         self.login()
