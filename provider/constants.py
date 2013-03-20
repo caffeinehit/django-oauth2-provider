6e1bf8b0c6dcebabedd063cc9edf39e5869c7188
Johan Meiring <johan@spiraleye.com>
2013-03-20 11:04:09 +0200

    Adds SINGLE_ACCESS_TOKEN constant (default is False)

--- provider/constants.py
+++ provider/constants.py
@@ -31,3 +31,5 @@ ENFORCE_SECURE = getattr(settings, 'OAUTH_ENFORCE_SECURE', False)
 ENFORCE_CLIENT_SECURE = getattr(settings, 'OAUTH_ENFORCE_CLIENT_SECURE', True)
 
 SESSION_KEY = getattr(settings, 'OAUTH_SESSION_KEY', 'oauth')
+
+SINGLE_ACCESS_TOKEN = getattr(settings, 'OAUTH_SINGLE_ACCESS_TOKEN', False)
