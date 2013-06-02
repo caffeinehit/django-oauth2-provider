9cf3c4435e425e1eba30d80e5cfac0a533b72a2b
Raymond Penners <raymond.penners@intenct.nl>
2013-06-02 22:42:31 +0200

    Allow cross-domain access to /oauth2/access_token/

--- provider/utils.py
+++ provider/utils.py
@@ -2,6 +2,7 @@ import hashlib
 import shortuuid
 from datetime import datetime
 from django.conf import settings
+from django.http import HttpResponse
 from .constants import EXPIRE_DELTA, EXPIRE_CODE_DELTA
 
 
@@ -40,3 +41,22 @@ def get_code_expiry():
     :attr:`datetime.timedelta` object.
     """
     return datetime.now() + EXPIRE_CODE_DELTA
+
+
+
+def cross_domain_ajax(func):
+    """ Sets Access Control request headers."""
+    HEADERS = {'Access-Control-Allow-Origin': '*', 
+               'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+               'Access-Control-Max-Age': 1000,
+               'Access-Control-Allow-Headers': 'X-Requested-With'}
+    def wrap(request, *args, **kwargs):
+        # Firefox sends 'OPTIONS' request for cross-domain javascript call.
+        if request.method != "OPTIONS": 
+            response = func(request, *args, **kwargs)
+        else:
+            response = HttpResponse()
+        for k, v in HEADERS.iteritems():
+            response[k] = v
+        return response
+    return wrap
