c65fe18e5e09185f061f48dea179521b9f730aa6
Brad Culberson <brad.culberson@mapmyfitness.com>
2013-11-13 12:34:00 -0700

    cleanup comment

--- provider/validators.py
+++ provider/validators.py
@@ -3,7 +3,7 @@ from django.core.validators import URLValidator
 
 def validate_uris(value):
     """
-    Validates the `value` contains valid space separated urls"
+    Validates the `value` contains valid space separated urls
     """
     v = URLValidator()
     for uri in value.split(" "):
