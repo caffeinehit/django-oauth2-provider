cdf4c8bd93e057066c6377bfee8199d7e4b0b0e1
Evan Culver <evan@evanculver.com>
2013-12-18 11:21:09 -0800

    Django 1.6 detection for test wrapper script.

--- test.sh
+++ test.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-python manage.py test provider oauth2 --traceback --failfast
+DJ_VERSION=$(django-admin.py --version)
 
+# exit if fail
+[[ "$?" -ne "0" ]] && exit;
 
+IS_16=$(echo $DJ_VERSION | grep "1.6")
+
+# if django version it's not 1.6 and so we pass different
+# app names to test runner
+if [ "$IS_16" = "1.6" ]; then
+    app_names=( provider provider.oauth2 )
+else
+    app_names=( provider oauth2 )
+fi
+
+python manage.py test ${app_names[@]} --traceback --failfast
