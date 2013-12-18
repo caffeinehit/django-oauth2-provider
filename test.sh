15346554203c64f2deecee01e6aaebd3a20495bf
Evan Culver <evan@evanculver.com>
2013-12-18 12:28:36 -0800

    Fixing silly bug in test wrapper's django version detection.

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ DJ_VERSION=$(django-admin.py --version)
 
 IS_16=$(echo $DJ_VERSION | grep "1.6")
 
-# if django version it's not 1.6 and so we pass different
+# if django version is not 1.6 (non-0 exit) we have to pass different
 # app names to test runner
-if [ "$IS_16" = "1.6" ]; then
+if [ "$?" -ne "1" ]; then
     app_names=( provider provider.oauth2 )
 else
     app_names=( provider oauth2 )
