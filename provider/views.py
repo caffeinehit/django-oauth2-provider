b63b27be60d6851a862051ed5b278642d57b721a
kusamau <m.nagni@gmail.com>
2014-01-17 16:01:21 +0000

    Revert "Implements the 'Implicit Grant'. A typical call looks like /oauth2/authorize/?client_id=an_existing_client_id=token&redirect_uri=the_client_redirect_uri"
    This reverts commit 728322173d3718e7cbbd8e216fbc22500d737b61.

--- provider/views.py
+++ provider/views.py
@@ -6,7 +6,6 @@ from django.utils.translation import ugettext as _
 from django.views.generic.base import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from oauth2.models import Client
-from oauth2.models import AccessToken as AccessTokenModel
 from . import constants, scope
 
 
@@ -262,22 +261,11 @@ class Authorize(OAuthView, Mixin):
         authorization_form = self.get_authorization_form(request, client,
             post_data, data)
 
-        if data.get('response_type', None):
-            if data.get('response_type') == 'code':                
-                if not authorization_form.is_bound \
-                    or not authorization_form.is_valid():
-                    return self.render_to_response({
-                                                    'client': client,
-                                                    'form': authorization_form,
-                                                    'oauth_data': data, })
-            #implements the 'implicit grant'
-            elif data.get('response_type') == 'token':
-                #uses request.user as the urls.py already required login_required
-                atm = AccessTokenModel.objects.create(user=request.user,
-                                                client=client,
-                                                scope=data.get('scope'))
-                at = AccessToken()
-                return at.access_token_response(atm, data)
+        if not authorization_form.is_bound or not authorization_form.is_valid():
+            return self.render_to_response({
+                'client': client,
+                'form': authorization_form,
+                'oauth_data': data, })
 
         code = self.save_authorization(request, client,
             authorization_form, data)
@@ -478,7 +466,7 @@ class AccessToken(OAuthView, Mixin):
         return HttpResponse(json.dumps(error), mimetype=mimetype,
                 status=status, **kwargs)
 
-    def access_token_response(self, access_token, data=None):
+    def access_token_response(self, access_token):
         """
         Returns a successful response after creating the access token
         as defined in :rfc:`5.1`.
@@ -498,13 +486,6 @@ class AccessToken(OAuthView, Mixin):
             response_data['refresh_token'] = rt.token
         except ObjectDoesNotExist:
             pass
-
-        if data.get('response_type') == 'token':
-            if len(data.get('state', '')) > 0:
-                response_data['state'] = data.get('state')
-            path = "%s?%s" % (data.get('redirect_uri'), 
-                              urlencode(response_data))                       
-            return HttpResponseRedirect(path)
 
         return HttpResponse(
             json.dumps(response_data), mimetype='application/json'
