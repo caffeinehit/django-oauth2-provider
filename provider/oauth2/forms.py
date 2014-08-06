a8714c2c87c4f795408fd8eafa2dbe1c3295ac61
JM Ibanez <jmibanez@gmail.com>
2014-08-06 11:42:18 +0800

    Handle module name changes for Django >=1.5
    To allow Python 3 compatibility, Django 1.5 renamed smart_unicode in
    django.utils.encoding as smart_text (as Python 3's str type is unicode
    by default). To accomodate running on earlier Django versions, we do an
    import of the old name; if we fail, we import smart_text as
    smart_unicode instead (with the assumption that Django is actually in
    the path).

--- provider/oauth2/forms.py
+++ provider/oauth2/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.contrib.auth import authenticate
-from django.utils.encoding import smart_unicode
+
+try:
+    from django.utils.encoding import smart_unicode
+except ImportError:
+    # Django >=1.5
+    from django.utils.encoding import smart_text as smart_unicode
+
 from django.utils.translation import ugettext as _
 from .. import scope
 from ..constants import RESPONSE_TYPE_CHOICES, SCOPES
