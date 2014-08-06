546637033233f4f0736e03f2b450c01026dcd053
JM Ibanez <jmibanez@gmail.com>
2014-08-06 11:38:41 +0800

    Use Python 3 syntax for exception handling.
    Breakage warning: this won't work on Python versions < 2.6.

--- provider/views.py
+++ provider/views.py
@@ -257,7 +257,7 @@ class Authorize(OAuthView, Mixin):
 
         try:
             client, data = self._validate_client(request, data)
-        except OAuthError, e:
+        except OAuthError as e:
             return self.error_response(request, e.args[0], status=400)
 
         authorization_form = self.get_authorization_form(request, client,
@@ -598,5 +598,5 @@ class AccessToken(OAuthView, Mixin):
 
         try:
             return handler(request, request.POST, client)
-        except OAuthError, e:
+        except OAuthError as e:
             return self.error_response(e.args[0])
