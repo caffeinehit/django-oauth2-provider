a7ae7bd67f9543ca7a8b25101249b52a622f6ca3
Beau Gunderson <beau@beaugunderson.com>
2014-09-20 17:07:12 -0700

    Fix tests for Django 1.7

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ DJ_VERSION=$(django-admin.py --version)
 # exit if fail
 [[ "$?" -ne "0" ]] && exit;
 
-IS_16=$(echo $DJ_VERSION | grep "1.6")
+IS_16_OR_17=$(echo $DJ_VERSION | grep -e "1.[67]")
 
 # if django version is not 1.6 (non-0 exit) we have to pass different
 # app names to test runner
