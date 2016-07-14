d8add9e2d6f67a1d044cb6f5268ad13bdadede6a
Chhay Tea <chhay@Chhays-MacBook-Pro.local>
2016-07-14 07:42:32 -0500

    Added utils file with method to create access and refresh tokens

---
+++ provider/oauth2/utils.py
@@ -0,0 +1,26 @@
+from ..constants import DEFAULT_SCOPES
+from ..scope import to_names
+from .models import Client
+from .models import RefreshToken
+from .models import AccessToken
+
+
+def create_access_and_refresh_token(client, user, scope=None):
+    scope = scope or DEFAULT_SCOPES[2][0] # read+write
+
+    access_token = AccessToken.objects.create(
+        client=client,
+        user=user,
+        scope=scope
+    )
+    refresh_token = RefreshToken.objects.create(
+        client=client,
+        user=user,
+        access_token=access_token,
+    )
+    return {
+        'access_token': access_token.token,
+        'refresh_token': refresh_token.token,
+        'expires_at': access_token.expires_text,
+        'scope': ' '.join(to_names(access_token.scope)),
+    }
