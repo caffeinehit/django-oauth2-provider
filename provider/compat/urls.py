33ca275cd0dceca8a63395e249200e07a7fd18b2
Valentín Moreno <valenmope@gmail.com>
2016-10-20 10:48:08 +0200

    adding compatibility with Django 10

--- provider/compat/urls.py
+++ provider/compat/urls.py
@@ -1,4 +1,1 @@
-try:
-    from django.conf.urls import patterns, url, include
-except ImportError: # django 1.3
-    from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import url, include
