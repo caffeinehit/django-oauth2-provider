4971a135e97233060d11a12f61fd3ae5130bcddc
Benjamin Goldberg <ben@spothero.com>
2016-11-10 16:57:43 -0600

    SH-6574 Remove patterns from compat import

--- provider/compat/urls.py
+++ provider/compat/urls.py
@@ -1,4 +1,4 @@
 try:
-    from django.conf.urls import patterns, url, include
+    from django.conf.urls import url, include
 except ImportError: # django 1.3
     from django.conf.urls.defaults import patterns, url, include
