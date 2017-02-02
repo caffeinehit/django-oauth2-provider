b3b0d47a5e5d20871d35b614bbc4bef1675da662
Chhay Tea <chhay@Chhays-MacBook-Pro.local>
2017-02-02 09:19:27 -0600

    Added expire_user_access_tokens util method

--- provider/oauth2/utils.py
+++ provider/oauth2/utils.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from django.utils.timezone import now
+
 from ..constants import DEFAULT_SCOPES
 from ..scope import to_names
 from .models import Client
@@ -24,3 +28,10 @@ def create_access_and_refresh_token(client, user, scope=None):
         'expires_at': access_token.expires_text,
         'scope': ' '.join(to_names(access_token.scope)),
     }
+
+
+def expire_user_access_tokens(user):
+    """Expire all active tokens associated with a user"""
+    current_ts = now()
+    expire_ts = current_ts - timedelta(minutes=1)
+    AccessToken.objects.filter(user=user, expires__gte=current_ts).update(expires=expire_ts)
